34cc0fa0754b6c208d1ad0a2859271790dd30bcf
Prosper NGOUARI <prosperngouari38@gmail.com>
2018-12-22 13:14:41 -0800

    Finition du blog

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,12 +2,50 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
+use App\Post;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use phpDocumentor\Reflection\DocBlock\Tag;
 
 class DashboardController extends Controller
 {
     public function index(){
-    	return view('admin.dashboard');
+
+         $posts = Post::all();
+
+         $popular_posts = Post::withCount('comments')
+            ->withCount('favorite_to_user')
+            ->orderBy('view_count', 'desc')
+            ->orderBy('comments_count', 'desc')
+            ->orderBy('favorite_to_user_count', 'desc')
+            ->take(5)->get();
+
+         $total_pending_posts = Post::where('is_approved', false)->count();
+
+         $all_views = Post::sum('view_count');
+
+         $author_count = User::where('role_id', 1)->count();
+
+         $new_authors_today = User::where('role_id', 1)
+                        ->whereDate('created_at', Carbon::today())->count();
+
+         $active_authors = User::where('role_id', 1)
+                        ->withCount('posts')
+                        ->withCount('comments')
+                        ->withCount('favorite_posts')
+                        ->orderBy('posts_count', 'desc')
+                        ->orderBy('comments_count', 'desc')
+                        ->orderBy('favorite_posts_count', 'desc')->get();
+
+         $category_count = Category::all()->count();
+
+
+
+
+    	return view('admin.dashboard', compact('posts', 'popular_posts', 'total_pending_posts', 'all_views',
+            'author_count', 'new_authors_today', 'active_authors', 'category_count'));
     }
 }
